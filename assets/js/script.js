9390176a61fd78eea561dd376ba5848bcecc00af
CharlottaG <>
2023-10-18 15:02:35 +0000

    Add styling to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,53 @@
-const modalLearnMore = document.getElementById('modal-learn-more');
-
 //Load DOM Content
 document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.getElementsByTagName('button');
     
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute('data-type') === 'btn-learn-more') {
-                modalLearnMore.style.display = 'block';
+            if (this.getAttribute('data-type') === 'gauge') {
+                modal.style.display = 'block';
             } else {
-                let calcOption = this.getAttribute('data-type');
-                alert(`You want to calculate ${calcOption}`);
-            }
+                let calcOption = this.getAttribute("data-type");
+                alert(`You clicked ${calcOption}`);
+            } 
         });
     }
 });
+
+const btnCalcGauge = document.getElementById('btn-calc-gauge');
+btnCalcGauge.addEventListener('click', calcGauge);
+
+const modal = document.getElementById('modal-gauge');
+const btnClose = document.getElementById('close-modal');
 
 let userSwatchStitches;
 let userSwatchRows;
 let numberOfStitchesPerCm;
 let numberOfRowsPerCm;
 
-let btnCalcGauge = document.getElementById('btn-calc-gauge');
 
-//btnCalcGauge.addEventListener('click', calcGauge);
-
-function openModalLearnMore() {
-    modalLearnMore.style.display = 'block';
+function closeModal() {
+    modal.style.display = 'none';
 };
 
-
 function calcGauge() {
-    StitchesPerCm;
+    stitchesPerCm;
+    rowsPerCm;
 }
 
-function StitchesPerCm() {
+function stitchesPerCm() {
     userSwatchStitches = document.getElementById('swatch-stitches').value;
     numberOfStitchesPerCm = userSwatchStitches / 10;
     document.getElementById('stitches-per-cm').innerHTML = numberOfStitchesPerCm;
-
 }
+
+function rowsPerCm() {
+    userSwatchRows = document.getElementById('swatch-rows').value;
+    numberOfRowsPerCm = userSwatchRows / 10;
+    document.getElementById('rows-per-cm').innerHTML = numberOfRowsPerCm;
+}
+
+
 
 // General calculations
 
