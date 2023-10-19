020bafd89d00b322173b5fe9fe53f3ce551df8a6
CharlottaG <>
2023-10-19 12:15:10 +0000

    Fix calculate button for increse-decrese calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //Load DOM Content
 document.addEventListener('DOMContentLoaded', function () {
-    let buttons = document.getElementsByTagName('button');
+    let buttons = document.getElementsByClassName('modal-button');
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute('data-type') === 'learn-more') {
-                modalLearnMore.style.display = 'block';
+            if (this.getAttribute('data-type') === 'gauge') {
+                modalGauge.style.display = 'block';
             } else {
                 let calcOption = this.getAttribute("data-type");
                 alert(`You clicked ${calcOption}`);
@@ -23,12 +23,13 @@ let desiredWidth;
 let desiredLength;
 let skeinLength;
 let rowsToKnit;
-let stitchesToCastOn;
+//let stitchesToCastOn;
+let currentStitches;
+let stitchesToIncreaseDecrease;
+let evenlyToIncreseDecrease;
 
-
-const modalLearnMore = document.getElementById('modal-learn-more');
-const modalCastOn = document.getElementById('modal-cast-on');
 const modalGauge = document.getElementById('modal-gauge');
+const modalCastOn = document.getElementById('modal-cast-on');
 const modalYarn = document.getElementById('modal-yarn');
 const modalIncreseDecrease = document.getElementById('modal-increase-decrease');
 
@@ -36,6 +37,8 @@ function closeModal() {
     modal.style.display = 'none';
 };
 
+//Swatch calculation for gauge
+//Function does not work
 function calcGauge() {
     stitchesPerCm;
     rowsPerCm;
@@ -53,18 +56,32 @@ function rowsPerCm() {
     document.getElementById('user-rows-per-cm').innerHTML = numberOfRowsPerCm;
 };
 
-function stitchesToCastOn() {
+btnCalcGauge = document.getElementById('btn-calc-gauge');
+btnCalcGauge.addEventListener('click', calcGauge);
+
+//Calculate cast-on stitches
+function calcCastOn() {
+    calcStitchesToCastOn;
+    calcRowsToKnit;
+}
+
+function calcStitchesToCastOn() {
     desiredWidth = document.getElementById('desired-width').value;
     numberOfStitchesToCastOn = desiredWidth * numberOfStitchesPerCm;
     document.getElementById('stitches-to-cast-on').innerHTML = numberOfStitchesToCastOn;
 };
 
-function rowsToKnit() {
+function calcRowsToKnit() {
     desiredLength = document.getElementById('desired-length').value;
     numberOfRowsToKnit = desiredLength * numberOfRowsPerCm;
     document.getElementById('rows-to-knit').innerHTML = numberOfRowsToKnit;
 };
 
+//Not working
+btnCalcCastOn = document.getElementById('btn-calc-cast-on');
+btnCalcCastOn.addEventListener('click', calcCastOn);
+
+//Calculate total number of skeins needed
 function calcTotalStitchesInMeter() {
     stitchesToCastOn = document.getElementById('rows-to-knit').value;
     rowsToKnit = document.getElementById('stitches-to-cast-on').value;
@@ -78,6 +95,25 @@ function calcNumberOfSkeins() {
     numberOfSkeins = totalStitches / skeinLength;
     document.getElementById('number-of-skeins').innerHTML = numberOfSkeins;    
 };
+
+//returns NaN
+btnCalcYarn = document.getElementById('btn-calc-yarn');
+btnCalcYarn.addEventListener('click', calcNumberOfSkeins);
+
+//Calculate how to increase or decrease - WORKS
+function calcIncreseDecrease() {
+    currentStitches = document.getElementById('current-stitch-count').value;
+    stitchesToIncreaseDecrease = document.getElementById('stitches-to-increase-decrease').value;
+    evenlyToIncreseDecrease = currentStitches / stitchesToIncreaseDecrease;
+    document.getElementById('evenly-increase-decrease').innerHTML = evenlyToIncreseDecrease;
+};
+
+btnCalcIncreseDecrease = document.getElementById('btn-calc-increase-decrease');
+btnCalcIncreseDecrease.addEventListener('click', calcIncreseDecrease);
+
+
+
+
 
 
 
