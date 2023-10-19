afba1e794574565c8001a69c46f3dab732a6c10b
CharlottaG <>
2023-10-19 16:20:10 +0000

    Remove unnecessary semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 //Load DOM Content
 document.addEventListener('DOMContentLoaded', function () {
-    let buttons = document.getElementsByClassName('modal-button'); //To only call modal opening buttons
+    var buttons = document.getElementsByClassName('modal-button'); //To only call modal opening buttons
 
-    for (let button of buttons) {
+    for (var button of buttons) {
         button.addEventListener("click", function () {
 
             if (this.getAttribute('data-type') === 'gauge') {
                 modalGauge.style.display = 'block';
             } else {
-                let calcOption = this.getAttribute("data-type");
+                var calcOption = this.getAttribute("data-type");
                 alert(`You clicked ${calcOption}`);
             }
         });
@@ -43,7 +43,7 @@ function openModal() {
 };
 
 //Close modal
-let btnCloseModal = document.getElementsByClassName('close-modal');
+var btnCloseModal = document.getElementsByClassName('close-modal');
 btnCloseModal.addEventListener('click', closeModal());
 
 function closeModal() {
@@ -51,12 +51,12 @@ function closeModal() {
 };*/
 
 //Swatch calculation for gauge
-let btnCalcGauge = document.getElementById('btn-calc-gauge');
+var btnCalcGauge = document.getElementById('btn-calc-gauge');
 btnCalcGauge.addEventListener('click', () => {
 
-    let userSwatchStitches = document.getElementById('user-swatch-stitches').value;
-    let userSwatchRows = document.getElementById('user-swatch-rows').value;
-    let num1 = 10;
+    var userSwatchStitches = document.getElementById('user-swatch-stitches').value;
+    var userSwatchRows = document.getElementById('user-swatch-rows').value;
+    var num1 = 10;
 
     stitchesPerCm(userSwatchStitches, num1);
     rowsPerCm(userSwatchRows, num1);
@@ -64,20 +64,20 @@ btnCalcGauge.addEventListener('click', () => {
 
 function stitchesPerCm(userSwatchStitches, num1) {
     document.getElementById('user-stitches-per-cm').innerHTML = divide(userSwatchStitches, num1);
-};
+}
 
 function rowsPerCm(userSwatchRows, num1) {
     document.getElementById('user-rows-per-cm').innerHTML = divide(userSwatchRows, num1);
-};
+}
 
 //Calculate cast-on stitches
-let btnCalcCastOn = document.getElementById('btn-calc-cast-on');
+var btnCalcCastOn = document.getElementById('btn-calc-cast-on');
 btnCalcCastOn.addEventListener('click', () => {
 
-    let desiredWidth = document.getElementById('desired-width').value;
-    let desiredLength = document.getElementById('desired-length').value;
-    let numberOfStitchesPerCm = document.getElementById('user-stitches-per-cm').textContent;
-    let numberOfRowsPerCm = document.getElementById('user-rows-per-cm').textContent;
+    var desiredWidth = document.getElementById('desired-width').value;
+    var desiredLength = document.getElementById('desired-length').value;
+    var numberOfStitchesPerCm = document.getElementById('user-stitches-per-cm').textContent;
+    var numberOfRowsPerCm = document.getElementById('user-rows-per-cm').textContent;
 
     calcStitchesToCastOn(desiredWidth, numberOfStitchesPerCm);
     calcRowsToKnit(desiredLength, numberOfRowsPerCm);
@@ -85,19 +85,19 @@ btnCalcCastOn.addEventListener('click', () => {
 
 function calcStitchesToCastOn(desiredWidth, numberOfStitchesPerCm) {
     document.getElementById('stitches-to-cast-on').innerHTML = multiply(desiredWidth, numberOfStitchesPerCm);
-};
+}
 
 function calcRowsToKnit(desiredLength, numberOfRowsPerCm) {
     document.getElementById('rows-to-knit').innerHTML = multiply(desiredLength, numberOfRowsPerCm);
-};
+}
 
 //Calculate total number of skeins needed
-let btnCalcYarn = document.getElementById('btn-calc-yarn');
+var btnCalcYarn = document.getElementById('btn-calc-yarn');
 btnCalcYarn.addEventListener('click', () => {
-    let stitchesToCastOn = document.getElementById('rows-to-knit').textContent;
-    let rowsToKnit = document.getElementById('stitches-to-cast-on').textContent;
-    let totalStitches = document.getElementById('project-yarn-length').textContent;
-    let skeinLength = document.getElementById('skein-length').value;
+    var stitchesToCastOn = document.getElementById('rows-to-knit').textContent;
+    var rowsToKnit = document.getElementById('stitches-to-cast-on').textContent;
+    var totalStitches = document.getElementById('project-yarn-length').textContent;
+    var skeinLength = document.getElementById('skein-length').value;
 
     calcTotalStitches(stitchesToCastOn, rowsToKnit);
     calcNumberOfSkeins(totalStitches, skeinLength);
@@ -105,18 +105,18 @@ btnCalcYarn.addEventListener('click', () => {
 
 function calcTotalStitches(stitchesToCastOn, rowsToKnit) {
     document.getElementById('project-yarn-length').innerHTML = multiply(stitchesToCastOn, rowsToKnit / 1000);
-};
+}
 
 function calcNumberOfSkeins(totalStitches, skeinLength) {
     document.getElementById('number-of-skeins').innerHTML = divide(totalStitches, skeinLength);
-};
+}
 
 //Calculate how to increase or decrease
-let btnCalcIncreseDecrease = document.getElementById('btn-calc-increase-decrease');
+var btnCalcIncreseDecrease = document.getElementById('btn-calc-increase-decrease');
 btnCalcIncreseDecrease.addEventListener('click', () => {
 
-    let currentStitches = document.getElementById('current-stitch-count').value;
-    let stitchesToIncreaseDecrease = document.getElementById('stitches-to-increase-decrease').value;
+    var currentStitches = document.getElementById('current-stitch-count').value;
+    var stitchesToIncreaseDecrease = document.getElementById('stitches-to-increase-decrease').value;
 
     calcIncreseDecrease(currentStitches, stitchesToIncreaseDecrease);
 
@@ -124,17 +124,17 @@ btnCalcIncreseDecrease.addEventListener('click', () => {
 
 function calcIncreseDecrease(currentStitches, stitchesToIncreaseDecrease) {
     document.getElementById('evenly-increase-decrease').innerHTML = divide(currentStitches, stitchesToIncreaseDecrease);
-};
+}
 
 // General calculations
 
 function multiply(num1, num2) {
     return Math.round(num1) * num2;
-};
+}
 
 function divide(num1, num2) {
     return Math.round(num1) / num2;
-};
+}
 
 
 
