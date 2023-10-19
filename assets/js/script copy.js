f7f7af0333b0486ef8e23cf37e7f60d34d51ea85
CharlottaG <>
2023-10-19 09:40:14 +0000

    Add calculations for cast-on and how many rows to knit

---
+++ assets/js/script copy.js
@@ -0,0 +1,143 @@
+//Load DOM Content
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute('data-type') === 'learn-more') {
+                modalLearnMore.style.display = 'block';
+            } else {
+                let calcOption = this.getAttribute("data-type");
+                alert(`You clicked ${calcOption}`);
+            }
+        });
+    }
+});
+
+//switch function for buttons in for loop
+switch (button) {
+    case 'gauge':
+        modalGauge.style.display = 'block';
+        break;
+    case 'cast-on':
+        modalCastOn.style.display = 'block';
+        break;
+    case 'yarn':
+        modalYarn.style.display = 'block';
+        break;
+    case 'increase-decrease':
+        modalIncreseDecrease.style.display = 'block';
+        break;
+};
+
+//let btnCalcCastOn = document.getElementById('btn-calc-cast-on');
+
+//btnCalcCastOn.addEventListener('click', () => {
+
+//    let desiredWidth = document.getElementById('desired-width').value;
+//    let desiredLength = document.getElementById('desired-length').value;
+
+//    calcCastOnStitches(desiredWidth, numberOfStitchesPerCm);
+//    calcKnitRows(desiredLength, numberOfRowsPerCm);
+//});
+
+//let metric = true;
+let userSwatchStitches;
+let userSwatchRows;
+let numberOfStitchesPerCm;
+let numberOfRowsPerCm;
+let numberOfStitchesToCastOn;
+//let desiredWidth;
+//let desiredLength;
+//var knitCalcOption = 'gauge'
+
+//const btnCalcGauge = document.getElementById('btn-calc-gauge');
+//btnCalcGauge.addEventListener('click', calcGauge);
+
+//Close modal btn
+//const btnClose = document.getElementById('close-modal');
+//btnClose.addEventListener('click', closeModal);
+
+const modalLearnMore = document.getElementById('modal-learn-more');
+const modalCastOn = document.getElementById('modal-cast-on');
+const modalGauge = document.getElementById('modal-gauge');
+const modalYarn = document.getElementById('modal-yarn');
+const modalIncreseDecrease = document.getElementById('modal-increase-decrease');
+
+//switch function for buttons in for loop
+switch (button) {
+    case 'gauge':
+        modalGauge.style.display = 'block';
+        break;
+    case 'cast-on':
+        modalCastOn.style.display = 'block';
+        break;
+    case 'yarn':
+        modalYarn.style.display = 'block';
+        break;
+    case 'increase-decrease':
+        modalIncreseDecrease.style.display = 'block';
+        break;
+};
+
+function closeModal() {
+    modal.style.display = 'none';
+};
+
+function calcGauge() {
+    stitchesPerCm;
+    rowsPerCm;
+};
+
+function stitchesPerCm() {
+    userSwatchStitches = document.getElementById('user-swatch-stitches').value;
+    numberOfStitchesPerCm = userSwatchStitches / 10;
+    document.getElementById('user-stitches-per-cm').innerHTML = numberOfStitchesPerCm;
+};
+
+function rowsPerCm() {
+    userSwatchRows = document.getElementById('user-swatch-rows').value;
+    numberOfRowsPerCm = userSwatchRows / 10;
+    document.getElementById('user-rows-per-cm').innerHTML = numberOfRowsPerCm;
+};
+
+function calcCastOn() {
+
+}
+
+function stitchesToCastOn() {
+    desiredWidth = document.getElementById('desired-width').value;
+    numberOfStitchesToCastOn = desiredWidth * userSwatchStitches;
+    document.getElementById('stitches-to-cast-on').innerHTML = numberOfStitchesToCastOn;
+};
+
+//let btnCalcCastOn = document.getElementById('btn-calc-cast-on');
+
+//btnCalcCastOn.addEventListener('click', () => {
+
+//    let desiredWidth = document.getElementById('desired-width').value;
+//    let desiredLength = document.getElementById('desired-length').value;
+
+//    calcCastOnStitches(desiredWidth, numberOfStitchesPerCm);
+//    calcKnitRows(desiredLength, numberOfRowsPerCm);
+//});
+
+
+function calcCastOnStitches(desiredWidth, numberOfStitchesPerCm) {
+    document.getElementById('user-stitches-to-cast-on').innerHTML = multiply(desiredWidth, numberOfStitchesPerCm);
+};
+
+function calcKnitRows(desiredLength, numberOfRowsPerCm) {
+    document.getElementById('user-rows-to-knit').innerHTML = multiply(desiredLength, numberOfRowsPerCm);
+};
+
+
+// General calculations
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function devide(num1, num2) {
+    return num1 / num2;
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute('data-type') === 'gauge') {
-                modal.style.display = 'block';
+            if (this.getAttribute('data-type') === 'learn-more') {
+                modalLearnMore.style.display = 'block';
             } else {
                 let calcOption = this.getAttribute("data-type");
                 alert(`You clicked ${calcOption}`);
@@ -14,78 +14,51 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-const btnCalcGauge = document.getElementById('btn-calc-gauge');
-btnCalcGauge.addEventListener('click', calcGauge);
-
-const modal = document.getElementById('modal-gauge');
-const btnClose = document.getElementById('close-modal');
-
-//let metric = true;
 let userSwatchStitches;
 let userSwatchRows;
 let numberOfStitchesPerCm;
 let numberOfRowsPerCm;
 let numberOfStitchesToCastOn;
+let desiredWidth;
+let desiredLength;
 
+
+const modalLearnMore = document.getElementById('modal-learn-more');
+const modalCastOn = document.getElementById('modal-cast-on');
+const modalGauge = document.getElementById('modal-gauge');
+const modalYarn = document.getElementById('modal-yarn');
+const modalIncreseDecrease = document.getElementById('modal-increase-decrease');
 
 function closeModal() {
     modal.style.display = 'none';
 };
 
 function calcGauge() {
-    stitchesPerCm();
-    rowsPerCm();
-}
+    stitchesPerCm;
+    rowsPerCm;
+};
 
 function stitchesPerCm() {
-    userSwatchStitches = document.getElementById('swatch-stitches').value;
+    userSwatchStitches = document.getElementById('user-swatch-stitches').value;
     numberOfStitchesPerCm = userSwatchStitches / 10;
-    document.getElementById('stitches-per-cm').innerHTML = numberOfStitchesPerCm;
-}
+    document.getElementById('user-stitches-per-cm').innerHTML = numberOfStitchesPerCm;
+};
 
 function rowsPerCm() {
-    userSwatchRows = document.getElementById('swatch-rows').value;
+    userSwatchRows = document.getElementById('user-swatch-rows').value;
     numberOfRowsPerCm = userSwatchRows / 10;
-    document.getElementById('rows-per-cm').innerHTML = numberOfRowsPerCm;
-}
-
-function calcCastOn() {
-
-}
+    document.getElementById('user-rows-per-cm').innerHTML = numberOfRowsPerCm;
+};
 
 function stitchesToCastOn() {
     desiredWidth = document.getElementById('desired-width').value;
-    numberOfStitchesToCastOn = desiredWidth * ;
+    numberOfStitchesToCastOn = desiredWidth * numberOfStitchesPerCm;
     document.getElementById('stitches-to-cast-on').innerHTML = numberOfStitchesToCastOn;
-}
+};
 
-let btnCalcCastOn = document.getElementById('btn-calc-cast-on');
+function rowsToKnit() {
+    desiredLength = document.getElementById('desired-length').value;
+    numberOfRowsToKnit = desiredLength * numberOfRowsPerCm;
+    document.getElementById('rows-to-knit').innerHTML = numberOfRowsToKnit;
+};
 
-btnCalcCastOn.addEventListener('click', () => {
-
-    let desiredWidth = document.getElementById('desired-width').value;
-    let desiredLength = document.getElementById('desired-length').value;
-
-    calcCastOnStitches(desiredWidth, numberOfStitchesPerCm);
-    calcKnitRows(desiredLength, numberOfRowsPerCm);
-});
-
-
-function calcCastOnStitches(desiredWidth, numberOfStitchesPerCm) {
-    document.getElementById('stitches-to-cast-on').innerHTML = multiply(desiredWidth, numberOfStitchesPerCm);
-}
-
-function calcKnitRows(desiredLength, numberOfRowsPerCm) {
-    document.getElementById('rows-to-knit').innerHTML = multiply(desiredLength, numberOfRowsPerCm);
-}
-
-
-// General calculations
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function devide(num1, num2) {
-    return num1 / num2;
-}
